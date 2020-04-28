a26e37d2f80e95f79f076a2f9a2270a8ba4e07a2
knb23\Ben <ben-orli.nathanson@meduniwien.ac.at>
2020-04-28 17:12:50 +0200

    foo, calculate responses and latencies, needs testing

--- random_walk.py
+++ random_walk.py
@@ -17,14 +17,17 @@ def random_walk(n_reps, n_samples, drift, sdrw):
     responses = np.repeat(0, n_reps)
     evidence = np.zeros((n_reps, n_samples + 1))
     # start unpacks generator (map output)
-    [*a] = map(lambda evi: foo(evi, n_samples, drift, sdrw), evidence)
+    [*a] = map(lambda trial_evidence: foo(trial_evidence, n_samples, drift, sdrw),
+               evidence, responses, latencies)
     return a
 
 
-def foo(evi, n_samples, drift, sdrw):
-    evi = np.cumsum(np.concatenate([np.zeros(1), np.random.normal(drift, sdrw, n_samples)]))
-    # TODO, p <− which ( abs (evidence[i ,])>criterion) [1] cross boundary
-    return evi
+def foo(trial_evidence, trial_response, trial_latency, n_samples, drift, sdrw):
+    trial_evidence = np.cumsum(np.concatenate([np.zeros(1), np.random.normal(drift, sdrw, n_samples)]))
+    trial_latency = np.where(abs(trial_evidence) > criterion)
+    trial_latency = list(zip(trial_latency[0], trial_latency[1]))[0]
+    trial_response = np.sign(trial_evidence[trial_latency])
+    return trial_evidence, trial_latency, trial_response
 
 
 print(random_walk(n_reps, n_samples, drift, sdrw))
