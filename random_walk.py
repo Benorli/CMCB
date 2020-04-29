007dc86fd96697b39607ad0df109c5b30cd0ee80
knb23\Ben <ben-orli.nathanson@meduniwien.ac.at>
2020-04-29 11:54:03 +0200

    name change sd_rw from sdrw and threshold from criterion

--- random_walk.py
+++ random_walk.py
@@ -4,23 +4,28 @@ import numpy as np
 
 # TODO consider adding an input for dataframe
 
-
-def random_walk(n_reps, n_samples, drift, sdrw):
+def random_walk(n_reps, n_samples, drift, sd_rw, threshold):
     """random walk model"""
 
-    latencies = np.repeat(0, n_reps)  # TODO: remove predefine, predefine not necessary with map
-    responses = np.repeat(0, n_reps)
-    evidence = np.zeros((n_reps, n_samples + 1))
-    # start unpacks generator (map output)
-    [*a] = map(lambda trial_evidence: foo(trial_evidence, n_samples, drift, sdrw),
-               evidence, responses, latencies)
+    # star unpacks generator (map output)
+    [*a] = map(lambda trial: foo(trial,
+                                 n_samples,
+                                 drift,
+                                 sd_rw,
+                                 threshold),
+               np.zeros((n_reps,
+                         n_samples + 1)))
     return a
 
 
-def foo(trial_evidence, trial_response, trial_latency, n_samples, drift, sdrw):
-    trial_evidence = np.cumsum(np.concatenate([np.zeros(1), np.random.normal(drift, sdrw, n_samples)]))
-    trial_latency = np.where(abs(trial_evidence) > criterion)
-    trial_latency = list(zip(trial_latency[0], trial_latency[1]))[0]
-    trial_response = np.sign(trial_evidence[trial_latency])
-    return trial_evidence, trial_latency, trial_response
+def foo(trial, n_samples, drift, sd_rw, threshold):
+    trial[:] = np.cumsum(np.concatenate([np.zeros(1),
+                                         np.random.normal(drift,
+                                                          sd_rw,
+                                                          n_samples)]))
+    trial_latency = np.where(abs(trial) > threshold)
+    trial_latency = list(zip(trial_latency[0],
+                             trial_latency[1]))[0]  # TODO error at this line
+    trial_response = np.sign(trial[trial_latency])
+    return trial, trial_latency, trial_response
 
