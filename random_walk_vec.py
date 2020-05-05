585e7b22e03446c09feaeb655b49c8da744fe3b2
knb23\Ben <ben-orli.nathanson@meduniwien.ac.at>
2020-05-05 14:37:42 +0200

    Add Random walk vectorized version, working progress

---
+++ random_walk_vec.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+
+def random_walk_vec(n_reps, n_samples, drift, sd_rw, threshold):
+    """ vectorzied random walk model with np"""
+
+    evidence = np.random.normal(0,
+                                0.3,
+                                [n_reps, n_samples]).cumsum(axis=1)
+    evidence = np.concatenate((np.zeros((n_reps, 1)), evidence),
+                              axis=1)
+    # TODO: Next np.which
+    # TODO: Use where to apply function depending on value
+    # TODO: numpy.put to replace values above threshold
