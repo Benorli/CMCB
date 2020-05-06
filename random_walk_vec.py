d7d629e4594deda1e78c3c191c42f953e3b3792d
knb23\Ben <ben-orli.nathanson@meduniwien.ac.at>
2020-05-06 20:23:43 +0200

    find when threshold crossed

--- random_walk_vec.py
+++ random_walk_vec.py
@@ -3,14 +3,26 @@ import pandas as pd
 
 
 def random_walk_vec(n_reps, n_samples, drift, sd_rw, threshold):
-    """ vectorzied random walk model with np"""
+    """ vectorized random walk model with np"""
 
     evidence = np.random.normal(0,
                                 0.3,
                                 [n_reps, n_samples]).cumsum(axis=1)
     evidence = np.concatenate((np.zeros((n_reps, 1)), evidence),
                               axis=1)
-    # TODO: Next np.which
-    # TODO: Use where to apply function depending on value
+    trial_latency = np.apply_along_axis(lambda x: np.where(x > threshold)[0][0], 1, np.abs(evidence))  # best solution for my purpose
+
+    # QUESTION: how can I do this when evidence sometimes doesn't contain value I look for with where?
+
+    # trial_latency = np.apply_along_axis(lambda x: np.where(x > threshold)[0][0], 1, evidence) ERROR
+    # trial_latency = np.apply_along_axis(lambda x: np.where(x > threshold)[0], 1, evidence) ERROR
+
+    # trial_latency = np.apply_along_axis(np.argmax, 1, evidence > threshold) gives this error:
+    # https://stackoverflow.com/questions/45765476/why-does-numpy-argmax-for-a-list-of-all-false-bools-yield-zero/45765513
+    # concatenated zeros prevent this at least
+
+    trial_response = np.sign(evidence[:, trial_latency])
+
+    # I will then put these into a dataframe and return them...
+
     # TODO: numpy.put to replace values above threshold
-    # TODO: which may require apply along axis (could include put here too)
