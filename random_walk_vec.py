717471588d85ff846ddfa20a8c68bc1379c2c75f
JoseGuzman <sjm.guzman@gmail.com>
2020-05-07 10:57:37 +0200

    add example to calculate latencies in normally distributed values

--- random_walk_vec.py
+++ random_walk_vec.py
@@ -66,8 +66,64 @@ def random_walk_vec(n_reps, n_samples, drift, sd_rw, threshold):
     # TODO: numpy.put to replace values above threshold
     # TODO: (jguzman) where is the return value of this function????
 
+def get_latencies(n_reps, n_samples, threshold):
+    """ 
+    Calculates the latencies (in number of samples) of crossing
+    threshodls of normaly distributed random variables with mean
+    zero and standard deviation 0.3.
+
+    Parameters
+    ----------
+    n_reps : int
+        number of normal distributions generated
+
+    n_samples : int
+        the number of samples drawn from a normal distribution
+
+    threshold : float
+        the value above which the latency will be calculated
+    
+    Returns
+    -------
+    A 1D NumPy array with the latency (in number of samples) of 
+    the crossing values. Note that the size of the array is n_reps.
+    """
+    mysize = (n_reps, n_samples)
+    mynorm = np.random.normal(loc = 0, scale = 0.3, size = mysize)
+
+    rows, cols = np.where(mynorm>threshold) # returns rows, cols
+
+    # we simply take the columns up to the number of repetitions (n_reps)
+    # because these are the first instances where the repetition was
+    # above the threshold
+    mylatency = cols[:n_reps] # we take up to the number of reps
+
+    return mylatency
 
 if __name__ == '__main__':
     # just for testing run the script with :
     # python random_walk_vect.py
+    import matplotlib.pyplot as plt
+
+    data = np.random.normal(loc = 0, scale = 0.3, size = (30, 5000))
+    mylat = get_latencies(n_reps = 30, n_samples = 1000, threshold=0.2)
+
+    fig, ax = plt.subplots(1, 2, figsize = (12, 8))
+
+    for trace in data:
+        # get first the histograms
+        values, base = np.histogram(trace, bins=40)
+        # compute cumulative 
+        mycum = np.cumsum(values)
+        ax[0].plot(base[:-1], mycum, c = 'gray', lw = 1)
+
+    ax[0].set_ylabel('Cumulative distribution')
+    ax[1].hist(mylat, bins = np.arange(0,200, 5))
+    ax[1].set_ylabel('Number of ocurrences')
+    ax[1].set_xlabel('Latency (in sampling points)')
+
+
+    fig.show()
+
+
 
