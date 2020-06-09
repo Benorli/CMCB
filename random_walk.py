9c103003bc7b60e0b211a525da43e29ef2dd2782
knb23\Ben <ben-orli.nathanson@meduniwien.ac.at>
2020-06-09 09:29:51 +0200

    Removing TODOs and removing an _ from 2 varnames

--- random_walk.py
+++ random_walk.py
@@ -1,20 +1,17 @@
 import numpy as np
 import pandas as pd
 
-# TODO, create as df?
-# TODO, np.vectorize > list comp > map > for, can this be implemented in a SIMPLE replicable way for diff models?
 
-
-def random_walk(n_reps, n_samples, drift, sd_rw, threshold):
+def random_walk(nreps, nsamples, drift, sd_rw, threshold):
     """random walk model"""
     # star unpacks generator (map output)
     [*list_trials] = map(lambda trial_evidence: simulate_trial(trial_evidence,
-                                                               n_samples,
+                                                               nsamples,
                                                                drift,
                                                                sd_rw,
                                                                threshold),
-                         np.zeros((n_reps,
-                                   n_samples + 1)))
+                         np.zeros((nreps,
+                                   nsamples + 1)))
     rw_df = pd.DataFrame(data=list_trials,
                          columns=['evidence_accumulation',
                                   'trial_latencies',
@@ -23,13 +20,13 @@ def random_walk(n_reps, n_samples, drift, sd_rw, threshold):
     return rw_df
 
 
-def simulate_trial(trial_evidence, n_samples, drift, sd_rw, threshold):
+def simulate_trial(trial_evidence, nsamples, drift, sd_rw, threshold):
     """simulate a single trial of the random walk model"""
     trial_evidence[:] = np.cumsum(np.concatenate([np.zeros(1),
                                                   np.random.normal(drift,
                                                                    sd_rw,
-                                                                   n_samples)]))
-    trial_latency = np.where(abs(trial_evidence) > threshold)  # TODO: Clear values following the threshold
+                                                                   nsamples)]))
+    trial_latency = np.where(abs(trial_evidence) > threshold)
     assert trial_latency[0].size, "No decision made, sd_rw too low or threshold too high"
     trial_response = np.sign(trial_evidence[trial_latency])
     return trial_evidence, trial_latency[0][1], trial_response[0]
