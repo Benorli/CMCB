2502145a86c1c30252c890512e2fa286f49a3b57
knb23\Ben <ben-orli.nathanson@meduniwien.ac.at>
2020-07-06 15:52:32 +0200

    plot randomwalk

--- sandbox.py
+++ sandbox.py
@@ -1,8 +1,13 @@
 from Random_walk.random_walk import random_walk as rw
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-rw_df = rw(nreps=20000,
-           nsamples=2000,
-           drift=0,
-           sd_rw=0.3,
-           threshold=3)
-print(rw_df)
+df_dv, df_trial_data = rw(nreps=2000,
+                          nsamples=1000,
+                          drift=0,
+                          sd_rw=0.3,
+                          threshold=3)
+
+sns.set(style="darkgrid")
+sns.lineplot(data=df_dv.iloc[:, 0:4], dashes=False)
+plt.show()
