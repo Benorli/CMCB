e7d7640b4c2fd884504b3317b99cfb7e7bc94535
Benorli <12201664+Benorli@users.noreply.github.com>
2020-10-27 18:05:08 +0100

    temporary changes, to work on from another machine

--- sandbox.py
+++ sandbox.py
@@ -1,13 +1,11 @@
-from Random_walk.random_walk import random_walk as rw
-import seaborn as sns
-import matplotlib.pyplot as plt
+# from Random_walk.random_walk import random_walk as rw
+import numpy as np
 
-df_dv, df_trial_data = rw(nreps=2000,
-                          nsamples=1000,
-                          drift=0,
-                          sd_rw=0.3,
-                          threshold=3)
 
-sns.set(style="darkgrid")
-sns.lineplot(data=df_dv.iloc[:, 0:4], dashes=False)
-plt.show()
+def every_more_than_each(every, each):
+    every_min = np.min(every)
+    more_than_all_min = each >= every_min
+    if any(more_than_all_min):
+        return False
+    else:
+        return True
